3512f76930cdab80bbf83cf529f80374d6d70729
somilsdiaz <somil.sandoval@icloud.com>
2024-11-08 01:07:02 -0500

    feat(HU-header):The link to clients was added from the navigation menu

--- src/components/ClientsTable.tsx
+++ src/components/ClientsTable.tsx
@@ -24,9 +24,8 @@ const ClientsTable = () => {
   const [updatingClientId, setUpdatingClientId] = useState<string | null>(null);
   const [updateError, setUpdateError] = useState<string | null>(null);
 
-  // Paginación
   const [currentPage, setCurrentPage] = useState(0);
-  const resultsPerPage = 8; // numeros por paginas
+  const resultsPerPage = 8;
 
   useEffect(() => {
     fetchClients();
@@ -68,7 +67,7 @@ const ClientsTable = () => {
       console.log('Enviando actualización:', clientToUpdate);
 
       const response = await fetch('https://web-fe-react-prj3-api.onrender.com/clients', {
-        method: 'POST',  // Cambiamos a POST según la API
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
@@ -86,7 +85,6 @@ const ClientsTable = () => {
       const responseData = await response.json();
       console.log('Respuesta de la API:', responseData);
 
-      // Actualizamos el estado local
       setClients(prevClients =>
         prevClients.map(client =>
           client.id === clientId
@@ -95,7 +93,6 @@ const ClientsTable = () => {
         )
       );
       
-      // Recargamos los datos para asegurar sincronización
       await fetchClients();
       
       setUpdateError(null);
@@ -108,12 +105,10 @@ const ClientsTable = () => {
   };
 
   const handleClientClick = (client: Client) => {
-    // Guardamos la información del cliente en localStorage para acceder a ella en la página de detalles
     localStorage.setItem('selectedClient', JSON.stringify(client));
     navigate('/ClientesDetails');
   };
 
-  // Lógica de paginación
   const currentClients = clients.slice(
     currentPage * resultsPerPage,
     (currentPage + 1) * resultsPerPage
@@ -128,88 +123,87 @@ const ClientsTable = () => {
   }
 
   return (
-    <div className="flex flex-col items-center p-4 overflow-hidden">
-      {error && (
-        <div className="mb-4 p-4 bg-red-100 text-red-700 rounded-md">
-          {error}
-        </div>
-      )}
-
-      {updateError && (
-        <div className="mb-4 p-4 bg-red-100 text-red-700 rounded-md">
-          {updateError}
-        </div>
-      )}
-
-      {/* Contenedor con scroll horizontal */}
-      <div className="overflow-x-auto max-w-full border border-gray-100 rounded-md shadow-xl">
-        <table className="table-auto bg-white w-full rounded-md">
-          <thead>
-            <tr className="bg-gray-200 text-gray-700 text-sm">
-              <th className="py-3 px-4 text-center font-semibold">NIT</th>
-              <th className="py-3 px-4 text-center font-semibold">Nombre</th>
-              <th className="py-3 px-4 text-center font-semibold">Dirección</th>
-              <th className="py-3 px-4 text-center font-semibold">Ciudad</th>
-              <th className="py-3 px-4 text-center font-semibold">País</th>
-              <th className="py-3 px-4 text-center font-semibold">Teléfono</th>
-              <th className="py-3 px-4 text-center font-semibold">Correo</th>
-              <th className="py-3 px-4 text-center font-semibold">Estado</th>
-              <th className="py-3 px-4 text-center font-semibold">Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentClients.map((client, index) => (
-              <tr 
-                key={client.id} 
-                className={`${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'} text-sm ${!client.active ? 'text-red-500' : 'text-gray-700'}`}
-              >
-                <td className="text-center w-[90px]">{client.nit}</td>
-                <td className="text-center w-[120px] cursor-pointer hover:text-blue-600 hover:underline" onClick={() => handleClientClick(client)}>{client.name}</td>
-                <td className="text-center w-[130px]">{client.address}</td>
-                <td className="text-center w-[90px]">{client.city}</td>
-                <td className="text-center w-[90px]">{client.country}</td>
-                <td className="text-center w-[100px]">{client.phone}</td>
-                <td className="text-center w-[200px]">{client.email}</td>
-                <td className={`text-center w-[100px] ${client.active ? 'text-green-500' : 'text-red-500'}`}>
-                  {client.active ? 'ACTIVO' : 'INACTIVO'}
-                </td>
-                <td className="text-center py-4 w-[250px]">
-                  <div className="flex justify-center gap-2">
-                    <button 
-                      className="bg-[#FF9800] text-white px-4 py-1 rounded-l-full flex items-center justify-center text-sm disabled:opacity-50 disabled:cursor-not-allowed"
-                      disabled={!client.active || updatingClientId === client.id}
-                    >
-                      Actualizar
-                    </button>
-                    <button 
-                      className={`${client.active ? 'bg-[#00BCD4]' : 'bg-green-500'} text-white px-4 py-1 rounded-r-full flex items-center justify-center text-sm disabled:opacity-50`}
-                      onClick={() => toggleClientStatus(client.id, !client.active)}
-                      disabled={updatingClientId === client.id}
-                    >
-                      {updatingClientId === client.id ? 'Procesando...' : client.active ? 'Inactivar' : 'Activar'}
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+<div className="flex flex-col items-center p-4">
+    {error && (
+      <div className="mb-4 p-4 bg-red-100 text-red-700 rounded-md">
+        {error}
       </div>
-      
-      {/* ReactPaginate */}
-      <ReactPaginate
-        previousLabel={<FaArrowLeft />} 
-        nextLabel={<FaArrowRight />} 
-        pageCount={Math.ceil(clients.length / resultsPerPage)}
-        onPageChange={handlePageChange}
-        containerClassName={'pagination flex mt-4 space-x-2 overflow-auto'}
-        pageClassName={'px-3 py-1 bg-gray-200 rounded'}
-        activeClassName={'bg-blue-500 text-white'}
-        previousClassName={'mt-2 rounded'}
-        nextClassName={'mt-2 rounded'}
-        disabledClassName={'opacity-50 cursor-not-allowed'}
-      />
+    )}
+
+    {updateError && (
+      <div className="mb-4 p-4 bg-red-100 text-red-700 rounded-md">
+        {updateError}
+      </div>
+    )}
+
+    {/* Contenedor con scroll horizontal */}
+    <div className="overflow-x-auto w-full border border-gray-100 rounded-md shadow-xl">
+      <table className="table-auto bg-white min-w-[800px] rounded-md"> {/* Cambié w-full por min-w-[800px] */}
+        <thead>
+          <tr className="bg-gray-200 text-gray-700 text-sm">
+            <th className="py-3 px-4 text-center font-semibold">NIT</th>
+            <th className="py-3 px-4 text-center font-semibold">Nombre</th>
+            <th className="py-3 px-4 text-center font-semibold">Dirección</th>
+            <th className="py-3 px-4 text-center font-semibold">Ciudad</th>
+            <th className="py-3 px-4 text-center font-semibold">País</th>
+            <th className="py-3 px-4 text-center font-semibold">Teléfono</th>
+            <th className="py-3 px-4 text-center font-semibold">Correo</th>
+            <th className="py-3 px-4 text-center font-semibold">Estado</th>
+            <th className="py-3 px-4 text-center font-semibold">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentClients.map((client, index) => (
+            <tr 
+              key={client.id} 
+              className={`${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'} text-sm ${!client.active ? 'text-red-500' : 'text-gray-700'}`}
+            >
+              <td className="text-center w-[90px]">{client.nit}</td>
+              <td className="text-center w-[120px] cursor-pointer hover:text-blue-600 hover:underline" onClick={() => handleClientClick(client)}>{client.name}</td>
+              <td className="text-center w-[130px]">{client.address}</td>
+              <td className="text-center w-[90px]">{client.city}</td>
+              <td className="text-center w-[90px]">{client.country}</td>
+              <td className="text-center w-[100px]">{client.phone}</td>
+              <td className="text-center w-[200px]">{client.email}</td>
+              <td className={`text-center w-[100px] ${client.active ? 'text-green-500' : 'text-red-500'}`}>
+                {client.active ? 'ACTIVO' : 'INACTIVO'}
+              </td>
+              <td className="text-center py-4 w-[250px]">
+                <div className="flex justify-center gap-2">
+                  <button 
+                    className="bg-[#FF9800] text-white px-4 py-1 rounded-l-full flex items-center justify-center text-sm disabled:opacity-50 disabled:cursor-not-allowed"
+                    disabled={!client.active || updatingClientId === client.id}
+                  >
+                    Actualizar
+                  </button>
+                  <button 
+                    className={`${client.active ? 'bg-[#00BCD4]' : 'bg-green-500'} text-white px-4 py-1 rounded-r-full flex items-center justify-center text-sm disabled:opacity-50`}
+                    onClick={() => toggleClientStatus(client.id, !client.active)}
+                    disabled={updatingClientId === client.id}
+                  >
+                    {updatingClientId === client.id ? 'Procesando...' : client.active ? 'Inactivar' : 'Activar'}
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
+    
+    <ReactPaginate
+      previousLabel={<FaArrowLeft />} 
+      nextLabel={<FaArrowRight />} 
+      pageCount={Math.ceil(clients.length / resultsPerPage)}
+      onPageChange={handlePageChange}
+      containerClassName={'pagination flex mt-4 space-x-2 overflow-auto'}
+      pageClassName={'px-3 py-1 bg-gray-200 rounded'}
+      activeClassName={'bg-blue-600 text-white font-semibold border border-blue-700'}
+      previousClassName={'mt-2 rounded'}
+      nextClassName={'mt-2 rounded'}
+      disabledClassName={'opacity-50 cursor-not-allowed'}
+    />
+  </div>
   );
 };
 
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -16,71 +16,63 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
 
     return (
         <div className="min-h-screen flex flex-col bg-gradient-to-r from-gray-100 to-gray-200 text-gray-900 font-sans antialiased">
-<header className="bg-gradient-to-r from-[#6366f1] to-blue-700 shadow-xl sticky top-0 z-50">
-  <div className="container mx-auto px-4 md:px-6 py-4 flex items-center justify-between relative">
-    {/* Contenedor del menú y la imagen */}
-    <div className="flex items-center space-x-4">
-      {/* Botón de menú de hamburguesa para pantallas pequeñas */}
-      <button 
-        onClick={toggleMenu}
-        className="text-white hover:text-gray-200 focus:outline-none lg:hidden" 
-        aria-label="Open navigation menu"
-      >
-        <svg className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-        </svg>
-      </button>
-
-      {/* Imagen para pantallas grandes */}
-      <div className="hidden lg:block">
-        <img 
-          src="./crm.png" 
-          alt="Imagen de ejemplo" 
-          className="h-12 w-12 rounded-full" 
-        />
-      </div>
-    </div>
-    
-    {/* Botón "CRM Pro" completamente centrado */}
-    <div className="absolute inset-0 flex items-center justify-center">
-      <button 
-        onClick={() => navigate('/')} 
-        className="text-lg md:text-2xl font-bold text-white tracking-wider transition-transform hover:scale-105 duration-300"
-      >
-        CRM Pro
-      </button>
-    </div>
-    
-    {/* Perfil de usuario alineado a la derecha */}
-    <div className="flex items-center space-x-4 text-white ml-auto">
-      <div className="hidden sm:flex flex-col items-end text-sm font-semibold">
-        <span className="hover:opacity-90">
-          John Doe
-        </span>
-        <span className="text-gray-300 text-xs hover:text-white">
-          JohnDoe@gmail.com
-        </span>
-      </div>
-      
-      <div className="relative group">
-        <UserCircle 
-          className="h-8 w-8 sm:h-10 sm:w-10 text-gray-200 transition-all duration-500 transform group-hover:rotate-12 group-hover:scale-110 group-hover:shadow-lg" 
-        />
-        <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-full blur-lg opacity-0 group-hover:opacity-40 transition-opacity duration-500"></div>
-      </div>
-    </div>
-  </div>
-</header>
-
+            <header className="bg-gradient-to-r from-[#6366f1] to-blue-700 shadow-xl sticky top-0 z-50">
+                <div className="container mx-auto px-4 md:px-6 py-4 flex items-center justify-between relative">
+                    <div className="flex items-center space-x-4">
+                        <button 
+                            onClick={toggleMenu}
+                            className="text-white hover:text-gray-200 focus:outline-none lg:hidden" 
+                            aria-label="Open navigation menu"
+                        >
+                            <svg className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+                        </button>
+                        <div className="hidden lg:block">
+                            <img 
+                            src="./crm.png" 
+                            alt="Imagen de ejemplo" 
+                            className="h-12 w-12 rounded-full" 
+                            />
+                        </div>
+                    </div>
+                    
+                    <div className="absolute inset-0 flex items-center justify-center">
+                        <button 
+                            onClick={() => navigate('/')} 
+                            className="text-lg md:text-2xl font-bold text-white tracking-wider transition-transform hover:scale-105 duration-300"
+                        >
+                            CRM Pro
+                        </button>
+                    </div>
+                
+                    <div className="flex items-center space-x-4 text-white ml-auto">
+                        <div className="hidden sm:flex flex-col items-end text-sm font-semibold">
+                            <span className="hover:opacity-90">
+                                John Doe
+                            </span>
+                            <span className="text-gray-300 text-xs hover:text-white">
+                                JohnDoe@gmail.com
+                            </span>
+                        </div>
+                
+                        <div className="relative group">
+                            <UserCircle 
+                            className="h-8 w-8 sm:h-10 sm:w-10 text-gray-200 transition-all duration-500 transform group-hover:rotate-12 group-hover:scale-110 group-hover:shadow-lg" 
+                            />
+                            <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-full blur-lg opacity-0 group-hover:opacity-40 transition-opacity duration-500"></div>
+                        </div>
+                    </div>
+                </div>
+            </header>
 
             <div className="flex flex-1">
-                {/* Menú de hamburguesa desplegable en modo responsive */}
                 {isMenuOpen && (
                     <nav className="lg:hidden bg-blue-700 p-4 absolute top-16 left-0 right-0 z-40 shadow-lg rounded-b-lg">
                         <ul className="space-y-2">
                             <li>
                                 <button 
-                                    onClick={() => navigate('/clients')}
+                                    onClick={() => navigate('/')}
                                     className="block w-full text-left text-white hover:bg-blue-800 p-2 rounded"
                                 >
                                     Clientes
@@ -118,7 +110,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
                     <nav aria-label="Sidebar" className="space-y-4">
                         <ul className="space-y-2">
                             {[
-                                { path: '/clients', icon: <Users className="h-6 w-6" />, label: 'Clientes' },
+                                { path: '/', icon: <Users className="h-6 w-6" />, label: 'Clientes' },
                                 { path: '/opportunities', icon: <Target className="h-6 w-6" />, label: 'Oportunidades' },
                                 { path: '/tracking', icon: <LineChart className="h-6 w-6" />, label: 'Seguimiento' },
                                 { path: '/dashboard', icon: <LayoutDashboard className="h-6 w-6" />, label: 'Dashboard' },
@@ -134,9 +126,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
                             ))}
                         </ul>
                     </nav>
-
-                    {/* Sección de actualización en el menú lateral */}
-                    <div className="pt-6 mt-6 border-t border-white/30">
+                    <div className="pt-6 mt-4 border-t border-white/30">
                         <div className="relative overflow-hidden rounded-lg bg-gradient-to-r from-yellow-300 to-yellow-500 shadow-lg p-6 transform transition-all duration-500 hover:scale-105">
                             <div className="absolute -top-4 -left-4 w-16 h-16 bg-yellow-400/60 rounded-full animate-pulse"></div>
                             <div className="absolute -top-6 -right-6 w-20 h-20 bg-yellow-600/40 rounded-full animate-pulse"></div>
@@ -156,9 +146,9 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
                     </div>
                 </aside>
 
-                <main className="flex-1 p-8 bg-gradient-to-r from-gray-50 to-gray-100 rounded-xl shadow-inner">
-                    {children}
-                </main>
+                <main className="flex-grow p-4 max-w-[1200px] mx-auto bg-gradient-to-r from-gray-50 to-gray-100 rounded-xl shadow-inner">
+                        {children}
+                    </main>
             </div>
 
             <footer className="bg-gray-900 text-white py-6 text-center shadow-inner mt-8">
